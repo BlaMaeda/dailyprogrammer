41c69c97d1a6dd71ab1db24e046fd4c5807f0616
Franco Victorio <victorio.franco@gmail.com>
2015-01-22 13:37:13 -0300

    198: Fixed some issued with Renzo's PR

--- 198_easy_words_with_enemies/main.hs
+++ 198_easy_words_with_enemies/main.hs
@@ -1,9 +1,10 @@
+import Data.List(sort)
 import qualified Data.Sequence as Seq
 
-collideSorted :: Ord a => Seq a -> Seq a -> (Seq a, Integer, Integer)
-collideSorted as0 bs0 = case (Seq.viewL as0, Seq.viewL bs0) of
-    (_, Seq.EmptyL) -> (as0, 0, Seq.size as0)
-    (Seq.EmptyL, _) -> (bs0, 0, Seq.size bs0)
+collideSorted :: Ord a => Seq.Seq a -> Seq.Seq a -> (Seq.Seq a, Int, Int)
+collideSorted as0 bs0 = case (Seq.viewl as0, Seq.viewl bs0) of
+    (_, Seq.EmptyL) -> (as0, Seq.length as0, 0)
+    (Seq.EmptyL, _) -> (bs0, 0, Seq.length bs0)
     (a Seq.:< as, b Seq.:< bs) -> case compare a b of
        EQ -> collideSorted as bs
        LT -> let (tailCol, aContrib, bContrib) = collideSorted as bs0
@@ -11,8 +12,8 @@ collideSorted as0 bs0 = case (Seq.viewL as0, Seq.viewL bs0) of
        GT -> let (tailCol, aContrib, bContrib) = collideSorted as0 bs
              in  (b Seq.<| tailCol, aContrib, 1 + bContrib)
     
-collide :: Ord a => [a] -> [a] -> (Seq a, Int, Int)
-collide as bs = collideSorted (Sec.fromList $ sort as) (Seq.fromList $ sort bs) 
+collide :: Ord a => [a] -> [a] -> (Seq.Seq a, Int, Int)
+collide as bs = collideSorted (Seq.fromList $ sort as) (Seq.fromList $ sort bs) 
 -- I think this can be written as: collide = collideSorted `on` (Seq.fromList . sort) -- `on` from Data.Function.on
 
 main :: IO ()
@@ -20,7 +21,7 @@ main = do
     (a:b:_) <- fmap words getLine
     let (collision, aContrib, bContrib) = collide a b
     putStrLn $ "Collision: " ++ show collision
-    putstrLn $ case compare aContrib bContrib of
+    putStrLn $ case compare aContrib bContrib of
        EQ -> "Tie"
        LT -> "Right wins"
        GT -> "Left wins"
