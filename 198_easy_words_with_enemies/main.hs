f223dc4a7f45d6f049454a8caf009fd27d70cbad
Renzo Carbonara <gnuk0001@gmail.com>
2015-01-22 13:10:07 -0300

    Update main.hs
    I used some pattern matching to clean up the readability of the code, and switched to using `Data.Sequence.Seq` to avoid a `O(n)` usage of `Data.List.length`. 
    
    I haven't actually compiled nor tested this code.

--- 198_easy_words_with_enemies/main.hs
+++ 198_easy_words_with_enemies/main.hs
@@ -1,32 +1,26 @@
-import Data.List
+import qualified Data.Sequence as Seq
 
-collideSorted :: (Eq a, Ord a) => [a] -> [a] -> ([a], Int, Int)
-collideSorted as [] = (as, length as, 0)
-collideSorted [] bs = (bs, 0, length bs)
-collideSorted (a:as) (b:bs) = 
-    if a == b then
-        let (tailCollision, aContrib, bContrib) = collideSorted as bs in 
-            (tailCollision, aContrib, bContrib)
-    else if a < b then
-        let (tailCollision, aContrib, bContrib) = collideSorted as (b:bs) in 
-            (a : tailCollision, 1 + aContrib, bContrib)
-    else
-        let (tailCollision, aContrib, bContrib) = collideSorted (a:as) bs in 
-            (b : tailCollision, aContrib, 1 + bContrib)
-
-collide :: (Eq a, Ord a) => [a] -> [a] -> ([a], Int, Int)
-collide as bs = collideSorted (sort as) (sort bs)
+collideSorted :: Ord a => Seq a -> Seq a -> (Seq a, Integer, Integer)
+collideSorted as0 bs0 = case (Seq.viewL as0, Seq.viewL bs0) of
+    (_, Seq.EmptyL) -> (as0, 0, Seq.size as0)
+    (Seq.EmptyL, _) -> (bs0, 0, Seq.size bs0)
+    (a Seq.:< as, b Seq.:< bs) -> case compare a b of
+       EQ -> collideSorted as bs
+       LT -> let (tailCol, aContrib, bContrib) = collideSorted as bs0
+             in  (a Seq.<| tailCol, 1 + aContrib, bContrib)
+       GT -> let (tailCol, aContrib, bContrib) = collideSorted as0 bs
+             in  (b Seq.<| tailCol, aContrib, 1 + bContrib)
+    
+collide :: Ord a => [a] -> [a] -> (Seq a, Int, Int)
+collide as bs = collideSorted (Sec.fromList $ sort as) (Seq.fromList $ sort bs) 
+-- I think this can be written as: collide = collideSorted `on` (Seq.fromList . sort) -- `on` from Data.Function.on
 
 main :: IO ()
 main = do
-    line <- getLine
-    lineWords <- return $ words line
-    (a, b) <- return $ (lineWords !! 0, lineWords !! 1)
-    (collision, aContrib, bContrib) <- return $ collide a b
-    putStrLn $ "Collision: " ++ collision
-    if aContrib > bContrib then
-        putStrLn "Left wins"
-    else if bContrib > aContrib then
-        putStrLn "Right wins"
-    else
-        putStrLn "Tie"
+    (a:b:_) <- fmap words getLine
+    let (collision, aContrib, bContrib) = collide a b
+    putStrLn $ "Collision: " ++ show collision
+    putstrLn $ case compare aContrib bContrib of
+       EQ -> "Tie"
+       LT -> "Right wins"
+       GT -> "Left wins"
